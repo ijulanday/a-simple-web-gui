330ebe4bc48132cb348ea4b28050d0f8b5ce91e1
ianulanday <ianulanday@email.arizona.edu>
2020-09-30 11:46:18 -0700

    added comments to main.go and cleaned up main.html a bit

--- app/main.go
+++ app/main.go
@@ -7,6 +7,7 @@ import (
     "github.com/go-redis/redis"
 )
 
+// unused redis db var
 var client *redis.Client
 
 type Comment struct {
@@ -14,6 +15,7 @@ type Comment struct {
     Comment string
 }
 
+// main page data struct
 type MainPage struct {
     Title string
     Subtitle string
@@ -23,6 +25,7 @@ type MainPage struct {
     Comments []Comment
 }
 
+// global main page var since db currently unimplemented (bad)
 var p MainPage = MainPage{
     Title: "Bad Discord", 
     Subtitle: "\"It's just worse\"",
@@ -32,12 +35,15 @@ var p MainPage = MainPage{
     Comments: []Comment{},
 }
 
+// sets up main page for get request
 func mainPageGetHandler(w http.ResponseWriter, r *http.Request) {
+    // some code for using redis database
     //comments, _ := client.LRange("comments", 0, 10).Result()
     t, _ := template.ParseFiles("templates/main.html")
     t.Execute(w,p)
 }
 
+// gathers discussion info and pushes data to html via template for post request
 func mainPagePostHandler(w http.ResponseWriter, r *http.Request) {
     r.ParseForm()
     c := Comment {
@@ -53,10 +59,13 @@ func mainPagePostHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+    //more code for redis database, left it in for implementation &
+    //  avoiding an unused package error
     client = redis.NewClient(&redis.Options{
         Addr: "localhost:6379",
     })
 
+    //using gorilla mux router
     r := mux.NewRouter()
     r.HandleFunc("/", mainPageGetHandler).Methods("GET")
     r.HandleFunc("/", mainPagePostHandler).Methods("Post")
